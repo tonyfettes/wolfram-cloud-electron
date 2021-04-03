4b267581f300bb07d2eb43f6f5df33ee5f2af525
Tony Fettes <tonyfettes@tonyfettes.com>
2021-04-04 00:54:51 +0800

    Initial commit

---
+++ packages/renderer/vite.config.js
@@ -0,0 +1,42 @@
+import {chrome} from '../../electron-vendors.config.json'
+import {join} from 'path'
+import externalPackages from '../../external-packages.config.js'
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {loadAndSetEnv} from '../../scripts/loadAndSetEnv.mjs'
+
+
+const PACKAGE_ROOT = __dirname
+
+/**
+ * Vite looks for `.env.[mode]` files only in `PACKAGE_ROOT` directory.
+ * Therefore, you must manually load and set the environment variables from the root directory above
+ */
+loadAndSetEnv(process.env.MODE, process.cwd())
+
+
+/**
+ * @see https://vitejs.dev/config/
+ */
+export default defineConfig({
+  root: PACKAGE_ROOT,
+  resolve: {
+    alias: {
+      '/@/': join(PACKAGE_ROOT, 'src') + '/',
+    },
+  },
+  plugins: [vue()],
+  base: '',
+  build: {
+    sourcemap: true,
+    target: `chrome${chrome}`,
+    polyfillDynamicImport: false,
+    outDir: 'dist',
+    assetsDir: '.',
+    rollupOptions: {
+      external: externalPackages,
+    },
+    emptyOutDir: true,
+  },
+})
+
