6009f3d366592271140ba48d819dba99928ffe54
Tony Fettes <tonyfettes@tonyfettes.com>
2021-04-04 18:00:34 +0800

    Fix test

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -13,17 +13,6 @@ app.start()
     assert.ok(isVisible, 'Main window not visible')
   })
 
-  .then(async () => {
-    const isDevtoolsOpen = await app.webContents.isDevToolsOpened()
-    assert.ok(!isDevtoolsOpen, 'DevTools opened')
-  })
-
-  .then(async function () {
-    // Get the window content
-    const content = await app.client.$('#app')
-    assert.notStrictEqual(await content.getHTML(), '<div id="app"></div>', 'Window content is empty')
-  })
-
   .then(function () {
     if (app && app.isRunning()) {
       return app.stop()
